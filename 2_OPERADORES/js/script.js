db1eb18db8684551754ef8aa9087c1fe5ea227db
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2023-09-09 19:16:22 -0300

    atualizado operadores cap 2

---
+++ 2_OPERADORES/js/script.js
@@ -0,0 +1,46 @@
+// 1 -NUMBER
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -125);
+
+// 2 - OPERADORES ARITMÉTICOS (+ - * /)
+console.log(2 + 4);
+console.log(5 + (4 * 12))
+console.log(89 / (1.7 * 1.7))
+console.log(2023 - 1988)
+
+// 3 - SPECIAL NUMBERS
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(12 * "dd")
+console.log(typeof NaN)
+
+//4 - STRINGS
+console.log("M")
+console.log("Mauricio")
+console.log("Rodrigues")
+console.log(`22`)
+console.log(typeof "Mauricio")
+console.log(typeof "Rodrigues")
+
+// 5 - Caracteres Especiais
+console.log('texto \n Duas linhas')
+console.log('Mauricio \nRodrigues')
+console.log('Espaçamento \t na tela')
+
+// 6 - CONCATENAÇÃO
+var a = 'Mauricio'
+var b = 'Rodrigues'
+console.log(a + " " + b)
+console.log('Me' + ' chamo' + ' Mauricio')
+console.log(`Me` + ` chamo` + ` Mauricio`)
+
+// 7 - INTERPOLAÇÃO (TEMPLATE STRINGS)
+console.log(`A soma de 2 + 2: ${2 + 2}`)
+console.log(`Podemos executar qualquer coisa aqui ${console.log('teste')}`)
+
+// 8 - BOOLEAM
+console.log(true)
+console.log(15 > 4)
+console.log(15 < 4)
+console.log(typeof Boolean)
